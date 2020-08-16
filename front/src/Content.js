91d55a67dbb12803cc16e977ae864e8904bc8171
Tomoconozaki <290651463@qq.com>
2020-08-16 20:07:33 +0800

    update

--- front/src/Content.js
+++ front/src/Content.js
@@ -97,37 +97,38 @@ export default function(props){
                 />
               </div>
               <div className="preview">
-                <p>No files for upload</p>
+                <p style={{color: 'red'}}>*暂无文件上传！</p>
               </div>
           </div>
           <div className="row-3">
             <button onClick={submitParam}>提交</button>
           </div>
+
+          <div className="desc">
+            <p className="frp1">数据计算说明</p>
+            <hr className="line-wht-short" />
+            <p className="frp2">
+              借鉴英国兽药残留委员会兽药残留风险排序矩阵。用毒性指标代替药性指标。膳食比例( 水蜜桃占居民总膳食的百分率) 以及农药毒效( 即ADI 值) 、使用频率、高暴露人群、残留水平5项指标均采用原赋值标准，进行风险排序
+            </p>
+          </div>
         </div>
+        
       </div>
       <div className="col-2">
-        <div className="row-1">
-          <p className="frp1">数据计算说明</p>
-          <hr className="line-wht-short" />
-          <p className="frp2">
-            借鉴英国兽药残留委员会兽药残留风险排序矩阵。用毒性指标代替药性指标。膳食比例( 水蜜桃占居民总膳食的百分率) 以及农药毒效( 即ADI 值) 、使用频率、高暴露人群、残留水平5项指标均采用原赋值标准，进行风险排序
-          </p>
-        </div>
-        <div className="row-2">
-          <div className="charts">
-            <div className="head">
-              <div>输出结果</div>
-              <div>
-                <div>图标展示数量</div>
-                <div>4</div>
-              </div>
+        <div className="charts">
+          <div className="head">
+            <div className="left-text">输出结果</div>
+            <div className="right-text">
+              <div>图标展示数量</div>
+              <div>4</div>
             </div>
-            <div className="container">
-              <div className="c-1"><Chart_1 locationData={locationData}/></div>
-              <div className="c-2"><Chart_2 checkedData={checkedData}/></div>
-              <div className="c-3"><Chart_3 sampleData={sampleData}/></div>
-              <div className="c-4"><Chart_4 weightData={weightData}/></div>
-            </div>
+          </div>
+          <div className="border"></div>
+          <div className="container">
+            <div className="c-1"><Chart_1 locationData={locationData}/></div>
+            <div className="c-2"><Chart_2 checkedData={checkedData}/></div>
+            <div className="c-3"><Chart_3 sampleData={sampleData}/></div>
+            <div className="c-4"><Chart_4 weightData={weightData}/></div>
           </div>
         </div>
       </div>
